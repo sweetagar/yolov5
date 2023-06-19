e594f716089ca159877ef86fbe2959987985736d
“ronsyao“ <164844136@qq.com>
2023-06-19 14:37:16 +0800

    debuging

---
+++ train_coco_classic80.sh
@@ -0,0 +1,14 @@
+#GPUS=6
+GPUS=1
+
+#32bit:
+python3 train.py \
+    --data data/coco.yaml\
+    --cfg models/yolov5s-classic-80.yaml \
+    --weights '' \
+    --batch-size 8 \
+    --hyp data/hyps/hyp.scratch-low.yaml \
+    --project ./runs/train/coco-32bit \
+    --epochs 100 \
+    --device 0
+
